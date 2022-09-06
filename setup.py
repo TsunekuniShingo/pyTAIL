844f458a50e900c40d6c012c902fc13e5124b468
Shingo at macbookair <s.tsunekuni@gmail.com>
2022-09-06 20:23:26 +0900

    edit setup

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
       name='pyTAIL',
       version='0.0',
       packages=find_packages(where='pyTAIL'),
+      package_dir={'':'pyTAIL'},
       include_package_data=True,
       install_requires=_requires_from_file('requirements.txt')
 )
